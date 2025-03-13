4cb3bb2d58dedd2c6b14f3a9797f4b7547821adc
zmac246 <maximilian.guhring.2023@live.rhul.ac.uk>
2025-03-13 17:31:18 +0000

    #58 navigation bar

---
+++ frontend/src/welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Welcome() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Some text</h1>
+      <p>Some text</p>
+    </div>
+  );
+}
+
+export default Welcome;
