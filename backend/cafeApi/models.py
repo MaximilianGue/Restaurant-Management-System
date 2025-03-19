fffb89462abff8fcf4fdc969f54007d026aa5e3d
Arion Caushi <Arion.Caushi.2023@live.rhul.ac.uk>
2025-03-19 13:47:30 +0000

    Manager model updated and implemented in Login/Register

--- backend/cafeApi/models.py
+++ backend/cafeApi/models.py
@@ -126,6 +126,7 @@ class User(AbstractUser):
     ROLE_CHOICES = [
         ('waiter', 'Waiter'),
         ('kitchen_staff', 'Kitchen Staff'),
+        ('manager', 'Manager'),
     ]
 
     role = models.CharField(max_length=20, choices=ROLE_CHOICES) 
@@ -134,11 +135,12 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.username} ({self.role}) - Staff ID: {self.staff_id}"
     
-    
 class Manager(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Staff_id = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"    
